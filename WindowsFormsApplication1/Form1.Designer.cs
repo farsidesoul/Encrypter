adcba5d72db17465905efc6b1f08fa548df54e11
Darron Lumley <dlumley@iinet.net.au>
2014-04-23 18:23:06 +1000

    Initial Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.setSecretKeyButton = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.secretKeyBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // setSecretKeyButton
+            // 
+            this.setSecretKeyButton.Location = new System.Drawing.Point(135, 21);
+            this.setSecretKeyButton.Name = "setSecretKeyButton";
+            this.setSecretKeyButton.Size = new System.Drawing.Size(96, 23);
+            this.setSecretKeyButton.TabIndex = 0;
+            this.setSecretKeyButton.Text = "Set Secret Key";
+            this.setSecretKeyButton.UseVisualStyleBackColor = true;
+            this.setSecretKeyButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(29, 80);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 1;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(155, 80);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 2;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // secretKeyBox
+            // 
+            this.secretKeyBox.Location = new System.Drawing.Point(29, 23);
+            this.secretKeyBox.Name = "secretKeyBox";
+            this.secretKeyBox.Size = new System.Drawing.Size(100, 20);
+            this.secretKeyBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 125);
+            this.Controls.Add(this.secretKeyBox);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.setSecretKeyButton);
+            this.Name = "Form1";
+            this.Text = "Encrypter/Decrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button setSecretKeyButton;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.TextBox secretKeyBox;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string sSecretKey;
+        string password;
+
+        public Form1()
+        {
+            
+            InitializeComponent();      
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            EncryptFile(@"C:\Users\ElephantPC\Desktop\TestFile.txt", @"C:\Users\ElephantPC\Desktop\EncryptedTestFile.txt", sSecretKey);
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            DecryptFile(@"C:\Users\ElephantPC\Desktop\EncryptedTestFile.txt", @"C:\Users\ElephantPC\Desktop\DencryptedTestFile.txt", sSecretKey);
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            sSecretKey = GenerateKey();
+        }
+
+        string GenerateKey()
+        {
+            password = secretKeyBox.Text;
+            PasswordDeriveBytes cdk = new PasswordDeriveBytes(password, null);
+            // Generate a DES key
+            byte[] iv = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] key = cdk.CryptDeriveKey("DES", "SHA1", 56, iv);
+            Console.WriteLine(key.Length * 8);
+            string str = System.Text.Encoding.ASCII.GetString(key);
+            return str;
+            
+        }
+
+        private void EncryptFile(string sInputFilename, string sOutputFilename, string sKey)
+        {
+            try
+            {
+                FileStream fsInput = new FileStream(sInputFilename, FileMode.Open, FileAccess.Read);
+                FileStream fsEncrypted = new FileStream(sOutputFilename, FileMode.Create, FileAccess.Write);
+
+                DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+
+                ICryptoTransform desencrypt = DES.CreateEncryptor();
+                CryptoStream cryptostream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write);
+                byte[] bytearrayinput = new byte[fsInput.Length - 1];
+                fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Close();
+                fsInput.Close();
+                fsEncrypted.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error, no password set.");
+            }
+            
+
+        }
+
+        private void DecryptFile(string sInputFilename, string sOutputFilename, string sKey)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            // Set secret key for DES algorithm
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            // Set initialization vector.
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+
+            // Create a file stream to read the encrypted file back.
+            FileStream fsread = new FileStream(sInputFilename, FileMode.Open, FileAccess.Read);
+
+            // Create a DES decryptor from the DES instance
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+            
+            //Create crypto stream set to read and do a DES decryption transform on incoming bytes
+            CryptoStream cryptostreamDecr = new CryptoStream(fsread, desdecrypt, CryptoStreamMode.Read);
+
+            //Print the contents of the decrpyted file
+            StreamWriter fsDecrypted = new StreamWriter(sOutputFilename);
+            try
+            {
+                fsDecrypted.Write(new StreamReader(cryptostreamDecr).ReadToEnd());
+            }
+            catch (CryptographicException de)
+            {
+                MessageBox.Show("Password is incorrect.");
+            }
+            
+            fsDecrypted.Flush();
+            fsDecrypted.Close();
+        }
+    }
+}
